9b21d5e21d471165f3f623c8ccaa6eede292cd9e
Maurice <maurice@betzel.net>
2016-03-23 18:08:33 +0100

    Update

--- src/main/java/net/betzel/bytedeco/javacv/document/Alignment.java
+++ src/main/java/net/betzel/bytedeco/javacv/document/Alignment.java
@@ -38,6 +38,7 @@ import java.util.List;
 
 import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
+import static org.bytedeco.javacpp.opencv_imgproc.minAreaRect;
 
 /**
  * Document Alignment with JavaCV
@@ -93,14 +94,38 @@ public class Alignment {
             // Ignore contours that are too small or too large
             // TODO make it relative to total image, requirement: look for A5 in A4
             if (area > 65536 && area < 131072) {
-                drawContours(bgr, contours, i, new Scalar(255, 0, 255, 0));
+                //drawContours(bgr, contours, i, new Scalar(255, 0, 255, 0));
                 double epsilon = 0.02 * arcLength(contours.get(i), true);
                 Mat approx = new Mat();
                 approxPolyDP(contours.get(i), approx, epsilon, true);
-                MatVector matVector = toPoint2fArray(approx);
-
-                //Point2f approx2f = new Point2f(approx.data());
-                polylines(bgr, matVector, true, new Scalar(255, 0, 255, 0));
+//                List<Point> point2fList = toPointList(approx);
+//                for(Point point : point2fList) {
+//                    System.out.println(point.x() + " " + point.y());
+//                }
+                Rect boundingRect = boundingRect(approx);
+                rectangle(bgr, boundingRect.tl(), boundingRect.br(), new Scalar(255, 0, 255, 0));
+                RotatedRect rotatedRect = minAreaRect(approx);
+                float angle = rotatedRect.angle();
+                Size2f rect_size = rotatedRect.size();
+                // rotate to landscape
+                Size newSize = new Size((int) rect_size.width(), (int) rect_size.height());
+                if(rect_size.width() < rect_size.height()) {
+                    angle += 90.0;
+                    newSize = new Size(newSize.height(), newSize.width());
+                }
+                Mat rotationMatrix = getRotationMatrix2D(rotatedRect.center(), angle, 1.0);
+                Mat rotated = new Mat(matrix.size(), matrix.type());
+                warpAffine(matrix, rotated, rotationMatrix, matrix.size());
+                showMatrix("Rotated", rotated);
+                Mat patch = new Mat();
+                //newSize.width(newSize.width() + ((newSize.width() / 100) * 24));
+                getRectSubPix(rotated, newSize, rotatedRect.center(), patch);
+                showMatrix("Cropped", patch);
+                //cvtColor(patch, patch, COLOR_BGR2GRAY);
+                Rect roi = new Rect(10, 10, patch.cols(), patch.rows());
+                Mat mask = matrix.apply(roi);
+                patch.copyTo(matrix, mask);
+                showMatrix("Result", matrix);
             }
         }
         showMatrix("Contours", bgr);
@@ -110,19 +135,17 @@ public class Alignment {
         //showMatrix(gray);
     }
 
-    private MatVector toPoint2fArray(Mat matrix) {
+    private List<Point> toPointList(Mat matrix) {
         if(matrix.checkVector(2) < 0 ) {
             throw new IllegalArgumentException("Expecting a vector Mat");
         }
         IntIndexer indexer = matrix.createIndexer();
         int size = (int) matrix.total();
-        List<Point2f> list = new ArrayList(size);
-        MatVector matVector = new MatVector(size);
+        List<Point> list = new ArrayList(size);
         for (int i = 0; i < size; i++) {
-            list.add(new Point2f(indexer.get(0, i, 0), indexer.get(0, i, 1)));
-            matVector.put(i, new Mat(new Point2f(indexer.get(0, i, 0), indexer.get(0, i, 1))));
+            list.add(new Point(indexer.get(0, i, 0), indexer.get(0, i, 1)));
         }
-        return matVector;
+        return list;
     }
 
 
